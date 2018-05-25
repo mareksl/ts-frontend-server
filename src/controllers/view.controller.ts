838aff3d30ff3c20ff9cacae0d87633210bca32a
mareksl <marekslad@hotmail.com>
2018-05-25 16:28:13 +0200

    add pagination to get handler (WIP)

--- src/controllers/view.controller.ts
+++ src/controllers/view.controller.ts
@@ -2,6 +2,17 @@ import { Request, Response } from 'express';
 import axios from 'axios';
 
 const restApi = process.env.REST_API;
+const eventsPerPageString = process.env.EVENTS_PER_PAGE || '5';
+const eventsPerPage = parseInt(eventsPerPageString);
+
+interface Event {
+  _id: string;
+  location: string;
+  date: number;
+  description: string;
+  link: string;
+  __v: string;
+}
 
 export const getImage = async (req: Request, res: Response) => {
   const id = req.params.id;
@@ -24,6 +35,8 @@ export const get = async (req: Request, res: Response) => {
   const getContacts = axios.get(`${restApi}/contacts`);
   const getImages = axios.get(`${restApi}/gallery`);
 
+  const page: number = parseInt(req.query['page']) || 0;
+
   try {
     const [events, members, contacts, images] = await Promise.all([
       getEvents,
@@ -31,10 +44,20 @@ export const get = async (req: Request, res: Response) => {
       getContacts,
       getImages
     ]);
+    const filteredEvents = {
+      upcoming: events.data.events.filter((event: Event) => {
+        return event.date >= Date.now();
+      }),
+      past: events.data.events
+        .filter((event: Event) => {
+          return event.date < Date.now();
+        })
+        .slice(page * eventsPerPage, (page + 1) * eventsPerPage)
+    };
 
     res.render('index.hbs', {
       title: 'Band',
-      eventList: events.data,
+      eventList: filteredEvents,
       memberList: members.data,
       contactList: contacts.data,
       galleryImageList: images.data
